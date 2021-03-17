cbb005735e478d020a23a6e548a58bce1ad02e97
Flowtter <naexys.naexys@gmail.com>
2021-03-17 16:46:33 +0000

    Removed views

--- Controllers/BikeController.cs
+++ Controllers/BikeController.cs
@@ -20,26 +20,6 @@ namespace DublinBike.Controllers
             _context = context;
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        public IActionResult Delete()
-        {
-            return View();
-        }
-
-        public IActionResult Details()
-        {
-            return View();
-        }
-
-        public IActionResult Edit()
-        {
-            return View();
-        }
-
         // GET: Bikes
         public async Task<IActionResult> Index(string searchString)
         {
