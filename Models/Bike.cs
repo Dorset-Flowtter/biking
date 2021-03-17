faa4a946ebcd3e2238314e889e1e8e362de4ef0e
Flowtter <naexys.naexys@gmail.com>
2021-03-17 16:24:07 +0000

    Edited bike model

--- Models/Bike.cs
+++ Models/Bike.cs
@@ -3,45 +3,50 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DublinBike.Models
 {
-	public class Bike
-	{
-		[Key]
-		[Required]
-		public int Number { get; set; }
+    public class Bike
+    {
+        [Key]
+        [Required]
+        public int Number { get; set; }
 
-		[Required]
-		[StringLength(60, MinimumLength = 3)]
-		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-		public string ContractName { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        public string ContractName { get; set; }
 
-		[Required]
-		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-		public string Name { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        public string Name { get; set; }
 
-		[Required]
-		[RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
-		[StringLength(50)]
-		public string Address { get; set; }
+        [Required]
+        [StringLength(256, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
+        public string Address { get; set; }
 
-		[Required]
-		[Range(-90, 90)]
-		public decimal Latitude { get; set; }
+        [Required]
+        [Range(-90, 90)]
+        public decimal Latitude { get; set; }
 
-		[Required]
-		[Range(-180, 180)]
-		public decimal Longitude { get; set; }
+        [Required]
+        [Range(-180, 180)]
+        public decimal Longitude { get; set; }
 
-		[Required]
-		public bool Banking { get; set; }
-		
-		public int AvailableBikes { get; set; }
+        [Required]
+        public bool Banking { get; set; }
 
-		public int AvailableStands { get; set; }
+        [Range(0, Int32.MaxValue)]
+        public int AvailableBikes { get; set; }
 
-		[Required]
-		public int Capacity { get; set; }
+        [Range(0, Int32.MaxValue)]
+        public int AvailableStands { get; set; }
 
-		[RegularExpression(@"^(OPEN|CLOSE)$")]
-		public string Status { get; set; }
-	}
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        public int Capacity { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(OPEN|CLOSE)$")]
+        public string Status { get; set; }
+    }
 }
