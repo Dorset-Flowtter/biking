6cf483720dda3d096e2545447eaacfc6740b75e0
Flowtter <naexys.naexys@gmail.com>
2021-03-17 16:17:11 +0000

    Comments

--- Controllers/BikeController.cs
+++ Controllers/BikeController.cs
@@ -47,7 +47,7 @@ namespace DublinBike.Controllers
 
         
 
-        // POST: Movies/Create
+        // POST: Bike/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
