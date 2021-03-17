b97f9b20a8bbc01de34f79f10a2b47bbb6bfd032
Pilpur <tanguy.maraux@gmail.com>
2021-03-17 14:42:47 +0000

    🚴‍♀️ Add Bike Class

---
+++ Models/Bike.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DublinBike.Models
+{
+	public class Bike
+	{
+		[Key]
+		[Required]
+		public int Number { get; set; }
+
+		[Required]
+		[StringLength(60, MinimumLength = 3)]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+		public string ContractName { get; set; }
+
+		[Required]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+		public string Name { get; set; }
+
+		[Required]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
+		[StringLength(50)]
+		public string Address { get; set; }
+
+		[Required]
+		[Range(0, 110.574)]
+		public decimal Latitude { get; set; }
+
+		[Required]
+		[Range(0, 111.320)]
+		public decimal Longitude { get; set; }
+
+		[Required]
+		public bool Banking { get; set; }
+		
+		public int AvailableBikes { get; set; }
+
+		public int AvailableStands { get; set; }
+
+		[Required]
+		public int Capacity { get; set; }
+
+		[RegularExpression(@"^(OPEN|CLOSE)$")]
+		public string Status { get; set; }
+	}
+}
